3cad450e1e5cc2c1fb9386abaee366c4c01af098
S Praveen Kumar <praveens.bec@gmail.com>
2019-12-25 02:12:13 +0530

    Add single and multi item dialogs

--- appessentials/src/main/java/dev/praveens/appessentials/EssDialogs.java
+++ appessentials/src/main/java/dev/praveens/appessentials/EssDialogs.java
@@ -8,7 +8,9 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 
+import androidx.annotation.ArrayRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 
 public class EssDialogs {
@@ -38,29 +40,100 @@ public class EssDialogs {
 
     public static void yesNoDialog(Context context, String title, String msg, @NonNull final YesNoCallback callback) {
         DialogInterface.OnClickListener listener = (dialog, which) -> callback.onResult(which == AlertDialog.BUTTON_POSITIVE);
-        createDialog(context, title, msg).setPositiveButton(android.R.string.yes, listener)
+        createDialog(context, title, msg).setCancelable(false).setPositiveButton(android.R.string.yes, listener)
                 .setNegativeButton(android.R.string.no, listener).create().show();
     }
 
     public static void yesNoDialog(Context context, @StringRes int titleId, @StringRes int msgId, @NonNull final YesNoCallback callback) {
         DialogInterface.OnClickListener listener = (dialog, which) -> callback.onResult(which == AlertDialog.BUTTON_POSITIVE);
-        createDialog(context, titleId, msgId).setPositiveButton(android.R.string.yes, listener)
+        createDialog(context, titleId, msgId).setCancelable(false).setPositiveButton(android.R.string.yes, listener)
                 .setNegativeButton(android.R.string.no, listener).create().show();
     }
 
     public static void yesNoDialog(Context context, String title, String msg, String yesText, String noText, @NonNull final YesNoCallback callback) {
         DialogInterface.OnClickListener listener = (dialog, which) -> callback.onResult(which == AlertDialog.BUTTON_POSITIVE);
-        createDialog(context, title, msg).setPositiveButton(yesText, listener)
+        createDialog(context, title, msg).setCancelable(false).setPositiveButton(yesText, listener)
                 .setNegativeButton(noText, listener).create().show();
     }
 
-    public static void yesNoDialog(Context context, String title, String msg, @StringRes int yesTextId, @StringRes int noTextId, @NonNull final YesNoCallback callback) {
+    public static void yesNoDialog(Context context, @StringRes int titleId, @StringRes int msgId, @StringRes int yesTextId, @StringRes int noTextId, @NonNull final YesNoCallback callback) {
         DialogInterface.OnClickListener listener = (dialog, which) -> callback.onResult(which == AlertDialog.BUTTON_POSITIVE);
-        createDialog(context, title, msg).setPositiveButton(yesTextId, listener)
+        createDialog(context, titleId, msgId).setCancelable(false).setPositiveButton(yesTextId, listener)
                 .setNegativeButton(noTextId, listener).create().show();
     }
 
+    public static void singleChoiceDialog(Context context, String title, @Nullable String msg, String[] entries, @NonNull final SingleItemCallback callback) {
+        final int[] selected = {0};
+        DialogInterface.OnClickListener listener = (dialog, which) -> {
+            if (which == AlertDialog.BUTTON_POSITIVE)
+                callback.onItemSelected(selected[0], entries[selected[0]]);
+            else if (which == AlertDialog.BUTTON_NEGATIVE)
+                callback.onCanceled();
+            else
+                selected[0] = which;
+        };
+        createDialog(context, title, msg).setSingleChoiceItems(entries, 0, (listener))
+                .setPositiveButton(android.R.string.ok, listener)
+                .setNegativeButton(android.R.string.cancel, listener)
+                .setOnCancelListener(dialog -> callback.onCanceled()).create().show();
+    }
+
+    public static void singleChoiceDialog(Context context, @StringRes int titleId, @StringRes int msgId, @ArrayRes int entries, @NonNull final SingleItemCallback callback) {
+        final int[] selected = {0};
+        DialogInterface.OnClickListener listener = (dialog, which) -> {
+            if (which == AlertDialog.BUTTON_POSITIVE)
+                callback.onItemSelected(selected[0], context.getResources().getStringArray(entries)[selected[0]]);
+            else if (which == AlertDialog.BUTTON_NEGATIVE)
+                callback.onCanceled();
+            else
+                selected[0] = which;
+        };
+        createDialog(context, titleId, msgId).setSingleChoiceItems(entries, 0, (listener))
+                .setPositiveButton(android.R.string.ok, listener)
+                .setNegativeButton(android.R.string.cancel, listener)
+                .setOnCancelListener(dialog -> callback.onCanceled()).create().show();
+    }
+
+    public static void multipleChoiceDialog(Context context, String title, @Nullable String msg, String[] entries, final boolean[] checked, @NonNull final MultipleItemCallback callback) {
+        DialogInterface.OnClickListener listener = (dialog, which) -> {
+            if (which == AlertDialog.BUTTON_POSITIVE)
+                callback.onSelected(checked);
+            else if (which == AlertDialog.BUTTON_NEGATIVE)
+                callback.onCanceled();
+        };
+        createDialog(context, title, msg).setMultiChoiceItems(entries, checked, (dialog, which, isChecked) -> checked[which] = isChecked)
+                .setPositiveButton(android.R.string.ok, listener)
+                .setNegativeButton(android.R.string.cancel, listener)
+                .setOnCancelListener(dialog -> callback.onCanceled()).create().show();
+    }
+
+    public static void multipleChoiceDialog(Context context, @StringRes int titleId, @StringRes int msgId, @ArrayRes int entries, final boolean[] checked, @NonNull final MultipleItemCallback callback) {
+        DialogInterface.OnClickListener listener = (dialog, which) -> {
+            if (which == AlertDialog.BUTTON_POSITIVE)
+                callback.onSelected(checked);
+            else if (which == AlertDialog.BUTTON_NEGATIVE)
+                callback.onCanceled();
+        };
+        createDialog(context, titleId, msgId).setMultiChoiceItems(entries, checked, (dialog, which, isChecked) -> checked[which] = isChecked)
+                .setPositiveButton(android.R.string.ok, listener)
+                .setNegativeButton(android.R.string.cancel, listener)
+                .setOnCancelListener(dialog -> callback.onCanceled()).create().show();
+    }
+
+
     interface YesNoCallback {
         void onResult(boolean yes);
+    }
+
+    interface SingleItemCallback {
+        void onItemSelected(int pos, String entry);
+
+        void onCanceled();
+    }
+
+    interface MultipleItemCallback {
+        void onSelected(boolean[] checked);
+
+        void onCanceled();
     }
 }
